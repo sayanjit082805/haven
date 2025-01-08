af0057e1fdf947c1bb465e4ce50fb9b285f9ff1a
sayanjit082805 <sayanjitukil2005@gmail.com>
2025-01-08 22:48:23 +0530

    add projects page

--- src/lib/data.js
+++ src/lib/data.js
@@ -25,3 +25,56 @@ export const project_data = [
     link: "https://github.com/sayanjit082805/QuoteStream",
   },
 ];
+
+export const all_projects = [
+  {
+    name: "TheMovieCentral",
+    description: "Search and fetch details of thousands of movies. Powered by the TMDB API.",
+    language: "Javascript",
+    link: "https://github.com/sayanjit082805/TheMovieCentral",
+  },
+  {
+    name: "AtmosAlert",
+    description:
+      "Get weather details of any city. Powered by the OpenWeatherMap API.",
+    language: "Javascript",
+    link: "https://github.com/sayanjit082805/AtmosAlert",
+  },
+  {
+    name: "Smart water management system",
+    description: "A collaborative hardware+software project, built using arduino microcontrollers (WIP)",
+    language: "Arduino",
+    link: "https://github.com/sayanjit082805/hydro-rangers-documentation",
+  },
+  {
+    name: "QuoteStream",
+    description: "Fetch random and motivational quotes. Powered by the API-Ninjas Quotes API",
+    language: "CSS",
+    link: "https://github.com/sayanjit082805/QuoteStream",
+  },
+  {
+    name: "FactFlow",
+    description: "Fetch random and fun facts. Powered by the API-Ninjas Facts API",
+    language: "Javascript",
+    link: "https://github.com/sayanjit082805/FactFlow",
+  },
+  {
+    name: ".dotfiles",
+    description: "My personal dotfiles for AwesomeWM",
+    language: "Lua",
+    link: "https://github.com/sayanjit082805/.dotfiles",
+  },
+  {
+    name: "PMSS Scholarship Portal",
+    description: "A collaborative project to streamline the process of scholarships for students",
+    language: "Javascript",
+    link: "https://github.com/sayanjit082805/TechRookies_sih_1728",
+  },
+  {
+    name: "AuraX",
+    description: "Another weather app, built using tkinter and OpenWeatherMap API",
+    language: "Python",
+    link: "https://github.com/wolfofdalalst/AuraX",
+  },
+  
+];
