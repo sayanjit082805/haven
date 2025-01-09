1775efa2201633a6411b3d513113979aeeb4c36e
sayanjit082805 <sayanjitukil2005@gmail.com>
2025-01-09 16:18:01 +0530

    add speed insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,20 @@ import mdx from "@astrojs/mdx";
 
 import { remarkReadingTime } from "./src/helpers/readingtime";
 
+import vercelStatic from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercelStatic({
+      webAnalytics: {
+          enabled: true,
+      },
+      // @ts-ignore
+      speedInsights: {
+          enabled: true,
+      },
+  }),
   integrations: [
     tailwind(),
     react(),
