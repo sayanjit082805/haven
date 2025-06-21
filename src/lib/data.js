fc3a05bbd5c93465128a998e5d7a9242234359e2
sayanjit082805 <sayanjitukil2005@gmail.com>
2025-06-21 20:25:46 +0530

    update projects

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,37 +1,69 @@
 export const project_data = [
   {
     name: "TheMovieCentral",
-    description: "Search and fetch details of thousands of movies. Powered by the TMDB API.",
+    description:
+      "Search and fetch details of thousands of movies. Powered by the TMDB API.",
     language: "Javascript",
     link: "https://github.com/sayanjit082805/TheMovieCentral",
   },
   {
-    name: "AtmosAlert",
+    name: "Flight Price Predictor",
     description:
-      "Get weather details of any city. Powered by the OpenWeatherMap API.",
-    language: "Javascript",
-    link: "https://github.com/sayanjit082805/AtmosAlert",
+      "A machine learning model to estimate the price of flight tickets.",
+    language: "Python",
+    link: "https://github.com/sayanjit082805/Flight-Price-Predictor",
   },
   {
     name: "Smart water management system",
-    description: "A collaborative hardware+software project, built using arduino microcontrollers (WIP)",
+    description:
+      "A collaborative hardware+software project, built using arduino microcontrollers (WIP)",
     language: "Arduino",
     link: "https://github.com/sayanjit082805/hydro-rangers-documentation",
   },
   {
-    name: "QuoteStream",
-    description: "Fetch random and motivational quotes. Powered by the API-Ninjas Quotes API",
-    language: "CSS",
-    link: "https://github.com/sayanjit082805/QuoteStream",
+    name: "Natural Scene Classifier",
+    description:
+      "A deep learning model to classify natural scenes using a CNN.",
+    language: "Python",
+    link: "https://github.com/sayanjit082805/Natural-Scene-Classifier",
   },
 ];
 
 export const all_projects = [
   {
     name: "TheMovieCentral",
-    description: "Search and fetch details of thousands of movies. Powered by the TMDB API.",
+    description:
+      "Search and fetch details of thousands of movies. Powered by the TMDB API.",
     language: "Javascript",
     link: "https://github.com/sayanjit082805/TheMovieCentral",
+  },
+   {
+    name: "Natural Scene Classifier",
+    description:
+      "A deep learning model to classify natural scenes using a CNN.",
+    language: "Python",
+    link: "https://github.com/sayanjit082805/Natural-Scene-Classifier",
+  },
+    {
+    name: "Flight Price Predictor",
+    description:
+      "A machine learning model to estimate the price of flight tickets.",
+    language: "Python",
+    link: "https://github.com/sayanjit082805/Flight-Price-Predictor",
+  },
+   {
+    name: "Stellar Classification",
+    description:
+      "A deep learning model to classify stars, galaxies and quasars using an ANN.",
+    language: "Python",
+    link: "https://github.com/sayanjit082805/Stellar-Classification",
+  },
+   {
+    name: "Aircraft-Classifier",
+    description:
+      "A machine learning model to classify the type of an aircraft.",
+    language: "Python",
+    link: "https://github.com/sayanjit082805/Aircraft-Classifier",
   },
   {
     name: "AtmosAlert",
@@ -42,19 +74,22 @@ export const all_projects = [
   },
   {
     name: "Smart water management system",
-    description: "A collaborative hardware+software project, built using arduino microcontrollers (WIP)",
+    description:
+      "A collaborative hardware+software project, built using arduino microcontrollers (WIP)",
     language: "Arduino",
     link: "https://github.com/sayanjit082805/hydro-rangers-documentation",
   },
   {
     name: "QuoteStream",
-    description: "Fetch random and motivational quotes. Powered by the API-Ninjas Quotes API",
+    description:
+      "Fetch random and motivational quotes. Powered by the API-Ninjas Quotes API",
     language: "CSS",
     link: "https://github.com/sayanjit082805/QuoteStream",
   },
   {
     name: "FactFlow",
-    description: "Fetch random and fun facts. Powered by the API-Ninjas Facts API",
+    description:
+      "Fetch random and fun facts. Powered by the API-Ninjas Facts API",
     language: "Javascript",
     link: "https://github.com/sayanjit082805/FactFlow",
   },
@@ -66,15 +101,16 @@ export const all_projects = [
   },
   {
     name: "PMSS Scholarship Portal",
-    description: "A collaborative project to streamline the process of scholarships for students",
+    description:
+      "A collaborative project to streamline the process of scholarships for students",
     language: "Javascript",
     link: "https://github.com/sayanjit082805/TechRookies_sih_1728",
   },
   {
     name: "AuraX",
-    description: "Another weather app, built using tkinter and OpenWeatherMap API",
+    description:
+      "Another weather app, built using tkinter and OpenWeatherMap API",
     language: "Python",
     link: "https://github.com/wolfofdalalst/AuraX",
   },
-  
 ];
